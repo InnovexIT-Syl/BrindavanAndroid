21bede11f7d0407a61752206c02938a76ece851e
raju31416 <mijanurrahman31416@gmail.com>
2019-10-19 23:53:09 +0600

    Service provider data Saved in Firestore

---
+++ app/src/main/java/net/innovexit/brindavan/Modles/ServiceProvider.java
@@ -0,0 +1,35 @@
+package net.innovexit.brindavan.Modles;
+
+import java.io.Serializable;
+
+public class ServiceProvider implements Serializable {
+
+    private String name, phoneNumber, address, workingExperience;
+
+    public ServiceProvider() {
+
+    }
+
+    public ServiceProvider(String name, String phoneNumber, String address, String workingExperience) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.workingExperience = workingExperience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getWorkingExperience() {
+        return workingExperience;
+    }
+}
